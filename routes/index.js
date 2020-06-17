0aabf75c3c9320ac49c710c3cc6d73bae1e804eb
Melissa Manousos <mmanousos@gmail.com>
2020-06-16 17:25:04 -0700

    Add map functionality

--- routes/index.js
+++ routes/index.js
@@ -64,7 +64,11 @@ router.post(
   catchErrors(authController.update)
 );
 
+// MAP
+router.get("/map", storeController.mapPage);
+
 // API
 router.get("/api/search", catchErrors(storeController.searchStores));
+router.get("/api/stores/near", catchErrors(storeController.mapStores));
 
 module.exports = router;
